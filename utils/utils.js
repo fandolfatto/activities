178f4cf51b4ca80778a0193abbf32c43e79f0e65
Frederique Andolfatto <pk37fki@eduvaud.ch>
2025-08-28 11:52:05 +0200

    math max for id to add

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@ const getNewID = activities => {
     //array of id
     const activitiesIds = activities.map((activity) => activity.id);
     //get max id
-    const maxId = activitiesIds.reduce((a, b) => Math.max(a, b));
+    const maxId = Math.max(...activitiesIds);
     const id = maxId + 1;
     return id;
 }
