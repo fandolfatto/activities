c5a3057200057e4a96011a81325f12d806044749
Frederique Andolfatto <pk37fki@eduvaud.ch>
2026-09-04 11:50:19 +0200

    change data for 2026 and adapt package.json

--- db/mock-activities.js
+++ db/mock-activities.js
@@ -3,13 +3,13 @@ let activities =
     {
       id: 1,
       name: "sortie ski",
-      date: "10.02.2026",
+      date: "08.02.2027",
       duration: "8"
     },
     {
       id: 2,
       name: "portes ouvertes",
-      date: "30.11.2025",
+      date: "28.11.2026",
       duration: "8"
     }
   ];
