229e43deff5848a6198c05855e85f40a355d2ad0
Frederique Andolfatto <pk37fki@eduvaud.ch>
2026-09-04 14:00:41 +0200

    use .env file and change some spelling errors

--- db/db-activities.js
+++ db/db-activities.js
@@ -1,54 +1,43 @@
 import mysql from 'mysql2/promise';
 
+process.loadEnvFile();
+
+const poolConn = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+})
+
 //I create a javascript literal object which contains methods, I will export it after having created it
 const db = {
 
-    connectToDB: async () => {
-        return await mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "P@ssw0rd",
-            port: 3308,
-            database: "app_activities",
-        });
-    },
-
     getAllActivities: async () => {
-        let con;
         try {
-            con = await db.connectToDB();
             //the getAllActivities function waits until the query is finished to execute
             //if there is some code after the call of this function, it will be executed without waiting the execution of this function
-            const [rows] = await con.query('SELECT * FROM activities');
+            const [rows] = await poolConn.execute('SELECT * FROM activities');
             return rows;
         } catch (err) {
             console.log(err);
             throw err;
-        } finally {
-            if (con) {await db.disconnectFromDatabase(con); }
         }
-
     },
 
     getActivityById: async ( id) => {
-        let con;
         try {
-            con = await db.connectToDB();
-            const [rows] = await con.query('SELECT * FROM activities WHERE id = ?', [id]);
+            const [rows] = await poolConn.execute('SELECT * FROM activities WHERE id = ?', [id]);
             return rows[0];
         } catch (err) {
             console.log(err);
             throw err;
-        } finally {
-            if (con) {await db.disconnectFromDatabase(con); }
         }
     },
 
     createActivity: async ( {name, activity_date, duration}) => {
-        let con;
         try {
-            con = await db.connectToDB();
-            const [result] = await con.query(
+            const [result] = await poolConn.execute(
                 'INSERT INTO activities (name, activity_date, duration) VALUES (?, ?, ?)',
                 [name, activity_date, duration]
             );
@@ -56,16 +45,12 @@ const db = {
         } catch (err) {
             console.log(err);
             throw err;
-        } finally {
-            if (con) {await db.disconnectFromDatabase(con); }
         }
     },
 
     updateActivity: async (id, {name, activity_date, duration}) => {
-        let con;
         try {
-            con = await db.connectToDB();
-            const [result] = await con.query(
+            const [result] = await poolConn.execute(
                 'UPDATE activities SET name = ?, activity_date = ?, duration = ? WHERE id = ?',
                 [name, activity_date, duration, id]
             );
@@ -74,16 +59,12 @@ const db = {
         } catch (err) {
             console.log(err);
             throw err;
-        } finally {
-            if (con) {await db.disconnectFromDatabase(con); }
         }
     },
 
     deleteActivity: async (id) => {
-        let con;
         try {
-            con = await db.connectToDB();
-            const [result] = await con.query('DELETE FROM activities WHERE id = ?', [id]);
+            const [result] = await poolConn.execute('DELETE FROM activities WHERE id = ?', [id]);
             if (result.affectedRows > 0) {
                 return {success: true};
             } else {
@@ -92,18 +73,6 @@ const db = {
         } catch (err) {
             console.log(err);
             throw err;
-        } finally {
-            if (con) {await db.disconnectFromDatabase(con); }
-        }
-    },
-
-    disconnectFromDatabase: async (connection) => {
-        try {
-            await connection.end();
-            console.log('Déconnexion de la base de données réussie');
-        } catch (error) {
-            console.error('Erreur lors de la déconnexion de la base de données :', error);
-            throw error;
         }
     }
 }
--- routes/activities.js
+++ routes/activities.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { db } from "../db/db-activities.js";
-import {isValidId, checkData} from "../helper.mjs";
+import {isValidId, checkData} from "../helper.js";
 
 // create a specific router for the activities, app.js won't be too big
 const activitiesRouter = express.Router();
@@ -22,7 +22,7 @@ activitiesRouter.get("/:id", async (req, res) => {
         }
         const activity = await db.getActivityById(id);
         if (activity === undefined) {
-            res.status(404).json({error: "Activitée non trouvée"});
+            res.status(404).json({error: "Activité non trouvée"});
         } else {
             res.json({activity});
         }
@@ -55,7 +55,7 @@ activitiesRouter.put('/:id', async (req, res) => {
     try {
         const resUpdateActivitiesNb = await db.updateActivity(req.params.id, req.body);
         if (resUpdateActivitiesNb === 0) {
-            res.status(404).json({error: "Activitée non trouvée"});
+            res.status(404).json({error: "Activité non trouvée"});
         } else {
             const activityUpdated = await db.getActivityById(req.params.id);
             res.json({message: 'Activity updated', updateActivity : {activityUpdated}});
@@ -74,9 +74,9 @@ activitiesRouter.delete('/:id', async (req, res) => {
         }
         let deletedActivity = await db.deleteActivity(req.params.id);
         if (deletedActivity.success) {
-            res.json({message: 'Activity deleted'});
+            res.json({message: 'Activité supprimée'});
         } else {
-            res.status(404).json({error: "Activitée non trouvée."});
+            res.status(404).json({error: "Activité non trouvée"});
         }
     } catch (error) {
         res.status(500).json({error: error.message});
